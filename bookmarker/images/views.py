9b3fb74123574304032943de15ee54973144b365
chetat <yekuwilfred@gmail.com>
2020-06-16 10:38:09 +0100

    Add image thumbnail and users like to detail page

--- bookmarker/images/views.py
+++ bookmarker/images/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
-
+from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def image_create(request):
@@ -27,5 +29,35 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
     return render(request,
                   'images/image/create.html',
-                  {'section': 'images',
-                   'form': form})
+                  {
+                      'section': 'images',
+                      'form': form
+                  })
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        'images/image/detail.html',
+        {'section': 'images',
+         'image': image}
+    )
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Exception as e:
+            print(e)
+            pass
+    return JsonResponse({'status': 'error'})
