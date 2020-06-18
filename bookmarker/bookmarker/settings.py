eda54e975cc2463039623e6e07d5dea9ebadc0e2
chetat <yekuwilfred@gmail.com>
2020-06-18 22:49:58 +0100

    Add aactions app

--- bookmarker/bookmarker/settings.py
+++ bookmarker/bookmarker/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'images.apps.ImagesConfig',
-    'easy_thumbnails',    
+    'easy_thumbnails',
+    'actions.apps.ActionsConfig'
 ]
 
 MIDDLEWARE = [
