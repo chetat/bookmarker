1652db6f8a61dcbb1a9b135f1d2e10d4d0cff4c3
chetat <yekuwilfred@gmail.com>
2020-06-19 23:58:19 +0100

    Implemented users action

--- bookmarker/account/views.py
+++ bookmarker/account/views.py
@@ -13,6 +13,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
 from .models import Contact
+from actions.utils import create_action
+from actions.models import Action
 
 
 def register(request):
@@ -25,6 +27,7 @@ def register(request):
             )
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -37,9 +40,18 @@ def register(request):
 
 @login_required
 def dashboard(request):
-    print(request.user.is_authenticated)
-    return render(request, 'account/dashboard.html',
-                  {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def user_login(request):
@@ -135,8 +147,8 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(
                     user_from=request.user,
-                    user_to=user
-                )
+                    user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_from=request.user,
--- bookmarker/images/views.py
+++ bookmarker/images/views.py
@@ -11,6 +11,9 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
+from actions.utils import create_action
+
+
 
 @login_required
 def image_create(request):
@@ -24,6 +27,8 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
+
             messages.success(request, 'Image added successfully')
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
@@ -59,6 +64,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
