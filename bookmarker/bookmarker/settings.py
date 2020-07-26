93add3a2a273ca4ad3f3b6966b2627863208ffc7
chetat <yekuwilfred@gmail.com>
2020-07-26 11:33:35 +0100

    Setup email for production

--- bookmarker/bookmarker/settings.py
+++ bookmarker/bookmarker/settings.py
@@ -14,7 +14,7 @@ import os
 from django.urls import reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+from dotenv import load_dotenv
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -23,9 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z=fb7p#@unv0fi!6s^+es!n7xvsm51)5u(w5+^v9d1=e^p!sgt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ["DEBUG"]
 
 
 # Application definition
@@ -142,7 +140,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-ALLOWED_HOSTS = ['yeku.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bookmarker.yekuwilfred.com', 'localhost', '127.0.0.1']
 
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy(
@@ -151,7 +149,14 @@ ABSOLUTE_URL_OVERRIDES = {
     )
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yekuwilfred@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Bookmarker App'
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
