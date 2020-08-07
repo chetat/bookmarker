09b7086d3d27207d125840e937ba67759c58100b
chetat <yekuwilfred@gmail.com>
2020-08-07 18:19:19 +0100

    Fix error related to collect static heroku

--- bookmarker/bookmarker/settings.py
+++ bookmarker/bookmarker/settings.py
@@ -167,5 +167,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
